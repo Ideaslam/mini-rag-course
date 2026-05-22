a57c2909d85823d9552c69839cbe6d7cf6710d96
Islam Hassan Abu Aish <islam.abuaish@gmail.com>
2026-05-22 16:18:32 +0300

    Refactor main.py to load environment variables and include base router; update requirements to add python-dotenv.

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from  fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv()
+from routes import base
+
 app = FastAPI()
-
-
-@app.get("/welcome")
-def welcome():
-    return {
-        "message": "Hello World"
-    }
-
-
+app.include_router(base.base_router)
